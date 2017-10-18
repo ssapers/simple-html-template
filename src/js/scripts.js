1093fd3345b053a19be477350c31e55349ce9740
Владислав Журавлёв <ssapers@gmail.com>
2017-10-18 17:00:23 +0300

    add files

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,35 @@
-// Your code here...
+const changeSize = (x) => {
+    const y = document.getElementById('size');
+    if(x === 1) {
+        y.innerHTML = '<span>Размер: <b>S</b></span> ';
+
+    }
+    if(x === 2) {
+        y.innerHTML = '<span>Размер: <b>M</b></span> ';
+
+    }
+    if(x === 3) {
+        y.innerHTML = '<span>Размер: <b>L</b></span> ';
+
+    }
+};
+const changeColor = (x) => {
+    const y = document.getElementById('color');
+    const z = document.getElementById('img');
+    if(x === 1) {
+        y.innerHTML = '<span>Цвет: <b>White</b></span> ';
+        z.innerHTML = '<img src="img/tshirt_white.jpg">';
+
+    }
+    if(x === 2) {
+        y.innerHTML = '<span>Цвет: <b>Yellow</b></span> ';
+        z.innerHTML = '<img src="img/tshirt_yellow.jpg">';
+
+    }
+    if(x === 3) {
+        y.innerHTML = '<span>Цвет: <b>Green</b></span> ';
+        z.innerHTML = '<img src="img/tshirt_green.jpg">';
+
+    }
+};
+
